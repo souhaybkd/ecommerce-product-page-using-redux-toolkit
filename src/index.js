f53ae842e40e2ad8fbef27dc2114215456ffbce9
souhaybkd <souh.kd@gmail.com>
2022-08-22 19:27:19 +0300

    some fixing

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import store from '../src/store/index.js'
-// import './index.css';
-
 import App from './App';
 
 
