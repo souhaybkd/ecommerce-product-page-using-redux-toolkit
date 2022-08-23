8da628c4dcdb7b69313cfc6d797e24e89d1a8ff3
souhaybkd <souh.kd@gmail.com>
2022-08-23 12:38:35 +0300

    add a test file

---
+++ build/precache-manifest.10969d599e844131088ddca38d28698a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "19b63ad065e8304dcc44",
+    "url": "/static/css/main.fabc8fe0.chunk.css"
+  },
+  {
+    "revision": "19b63ad065e8304dcc44",
+    "url": "/static/js/main.19b63ad0.chunk.js"
+  },
+  {
+    "revision": "90af895e1b3da04d1e79",
+    "url": "/static/js/1.90af895e.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0ac65e8df3cdb3240e82aac50d5b84c5",
+    "url": "/static/media/image-product-1.0ac65e8d.jpg"
+  },
+  {
+    "revision": "dc5f56d918a4a172b00d9fb5c782f873",
+    "url": "/static/media/image-product-2.dc5f56d9.jpg"
+  },
+  {
+    "revision": "d8f0fd258d7833d69417c1cc12d217bb",
+    "url": "/static/media/image-product-3.d8f0fd25.jpg"
+  },
+  {
+    "revision": "5e95fcb499ff1a1c449b25f41fc5bf1b",
+    "url": "/static/media/image-product-4.5e95fcb4.jpg"
+  },
+  {
+    "revision": "2fff91da1d14257def961611b3d930e4",
+    "url": "/index.html"
+  }
+];
